5b52051578d5b004024f5e16529690c68218920a
Edward Wei Liao <edwardweiliao@gmail.com>
2021-07-22 10:30:56 +0800

    comment out more logs

--- scripts/webRtcPlayer.js
+++ scripts/webRtcPlayer.js
@@ -97,7 +97,7 @@
                 }
 
                 datachannel.onmessage = function (e) {
-                  console.log(`Got message (${label})`, e.data)
+                  // console.log(`Got message (${label})`, e.data)
                   if (self.onDataChannelMessage)
                     self.onDataChannelMessage(e.data);
                 }
@@ -152,7 +152,7 @@
                 //console.log('Printing Stats');
 
                 let newStat = {};
-                console.log('----------------------------- Stats start -----------------------------');
+                // console.log('----------------------------- Stats start -----------------------------');
                 stats.forEach(stat => {
 //                    console.log(JSON.stringify(stat, undefined, 4));
                     if (stat.type == 'inbound-rtp' 
